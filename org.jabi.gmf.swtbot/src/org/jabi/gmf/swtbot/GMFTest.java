7747ef9531565fa549fe50fcb05f4a6061524e6e
Superkiki <zegogo93@gmail.com>
2014-03-01 10:16:47 +0100

    succeed to open a diagram

--- org.jabi.gmf.swtbot/src/org/jabi/gmf/swtbot/GMFTest.java
+++ org.jabi.gmf.swtbot/src/org/jabi/gmf/swtbot/GMFTest.java
@@ -4,6 +4,7 @@ import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.keyboard.Keyboard;
 import org.eclipse.swtbot.swt.finder.keyboard.KeyboardFactory;
 import org.eclipse.swtbot.swt.finder.keyboard.Keystrokes;
+import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,23 +16,32 @@ import fr.obeo.dsl.viewpoint.tests.swtbot.support.api.editor.SWTBotDesignerHelpe
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class GMFTest {
 
+	private static final String SAMPLE_PATH = "C:\\ObeoDesigner-6.2-win32.win32.x86_64\\ObeoDesigner\\wk\\org.jabi.gmf.sample.zip";
 	private static SWTDesignerBot bot = new SWTDesignerBot();
 	private static Keyboard keyboard = KeyboardFactory.getSWTKeyboard();
 
 	@BeforeClass
 	public static void beforeClass() throws Exception {
+		SWTBotPreferences.TIMEOUT = 10000;
+		bot.button("OK").click();
 		bot.viewByTitle("Welcome").close();
+	}
 
+	@Test
+	public void test() {
 		bot.menu("Import...").click();
 		bot.tree().getTreeItem("General").expand();
 		bot.tree().getTreeItem("General")
 				.getNode("Existing Projects into Workspace").select();
 		bot.button("Next >").click();
 		bot.radio("Select archive file:").click();
-		bot.text(1).setText("/home/hsellou/workspaces/org.jabi.gmf.sample.zip");
+		bot.text(1).setText(SAMPLE_PATH);
 		keyboard.pressShortcut(Keystrokes.CR);
 		bot.button("Finish").click();
-		bot.viewByTitle("Model Explorer").setFocus();
+		bot.text("type filter text").setFocus();
+		keyboard.pressShortcut(Keystrokes.TAB);
+		bot.tree();
+		bot.tree().getTreeItem("org.jabi.gmf.sample").select();
 		bot.tree().getTreeItem("org.jabi.gmf.sample").expand();
 		bot.tree().getTreeItem("org.jabi.gmf.sample")
 				.getNode("representations.aird").expand();
@@ -39,22 +49,20 @@ public class GMFTest {
 				.getNode("representations.aird").getNode("mymodelViewpoint")
 				.expand();
 		bot.tree().getTreeItem("org.jabi.gmf.sample")
+				.getNode("representations.aird").click();
+		bot.tree().getTreeItem("org.jabi.gmf.sample")
 				.getNode("representations.aird").getNode("mymodelViewpoint")
 				.getNode("MyModel diagram").expand();
 		bot.tree().getTreeItem("org.jabi.gmf.sample")
 				.getNode("representations.aird").getNode("mymodelViewpoint")
-				.getNode("MyModel diagram").getNode("MyModel diagram").select();
+				.getNode("MyModel diagram").expand();
 		bot.tree().getTreeItem("org.jabi.gmf.sample")
 				.getNode("representations.aird").getNode("mymodelViewpoint")
-				.getNode("MyModel diagram").getNode("MyModel diagram").click();
+				.getNode("MyModel diagram").getNode("MyModel diagram")
+				.doubleClick();
 
-		SWTBotDesignerEditor editor = SWTBotDesignerHelper.getDesignerEditor("MyModel diagram");
-
-	}
-
-	@Test
-	public void test() {
-
+		SWTBotDesignerEditor editor = SWTBotDesignerHelper
+				.getDesignerEditor("MyModel diagram");
 	}
 
 }
